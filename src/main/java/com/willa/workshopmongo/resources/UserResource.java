5cef222936a8bafa62d210420c5049ff339ba869
willa <willaevangelista.pro@gmail.com>
2026-08-08 19:31:05 -0300

    Deleção de usuário com Delete

--- src/main/java/com/willa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/willa/workshopmongo/resources/UserResource.java
@@ -44,4 +44,10 @@ public class UserResource {
 
         return ResponseEntity.created(uri).build();
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
