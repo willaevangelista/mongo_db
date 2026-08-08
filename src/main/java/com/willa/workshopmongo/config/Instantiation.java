9504fdb2846816cf577ac5e36d8008ed365dc19b
willa <willaevangelista.pro@gmail.com>
2026-08-08 14:48:08 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/willa/workshopmongo/config/Instantiation.java
+++ src/main/java/com/willa/workshopmongo/config/Instantiation.java
@@ -20,8 +20,8 @@ public class Instantiation implements CommandLineRunner {
         userRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
-        User alex = new User(null, "Maria Brown", "maria@gmail.com");
-        User bob = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
     }
--- src/main/java/com/willa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/willa/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.willa.workshopmongo.resources;
 
 import com.willa.workshopmongo.domain.User;
+import com.willa.workshopmongo.dto.UserDTO;
 import com.willa.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +20,11 @@ public class UserResource {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<User> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list).getBody();
+        List<UserDTO> listDTO = list.stream()
+                                .map(UserDTO::new)
+                                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
