5a1ba4fd185121ca0cd8072d38046dbb6354003b
willa <willaevangelista.pro@gmail.com>
2026-08-08 19:24:02 -0300

    Inserção de usuário com POST

--- src/main/java/com/willa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/willa/workshopmongo/resources/UserResource.java
@@ -6,10 +6,10 @@ import com.willa.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,12 +18,12 @@ public class UserResource {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
         List<UserDTO> listDTO = list.stream()
                                 .map(UserDTO::new)
-                                .collect(Collectors.toList());
+                                .toList();
         return ResponseEntity.ok().body(listDTO);
     }
 
@@ -31,5 +31,17 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+
+        User user = userService.fromDTO(userDTO);
+        user = userService.insert(user);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/willa/workshopmongo/services/UserService.java
+++ src/main/java/com/willa/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.willa.workshopmongo.services;
 
 import com.willa.workshopmongo.domain.User;
+import com.willa.workshopmongo.dto.UserDTO;
 import com.willa.workshopmongo.repository.UserRepository;
 import com.willa.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,19 @@ public class UserService {
     private UserRepository userRepository;
 
     public List<User> findAll() {
-        List<User> list = userRepository.findAll();
-        return list;
+        return userRepository.findAll();
     }
 
     public User findById(String id) {
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User user) {
+        return userRepository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
