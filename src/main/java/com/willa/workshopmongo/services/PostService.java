94f88d8247a33fa16a1111c7b87757d83abb08c1
willa <willaevangelista.pro@gmail.com>
2026-08-09 19:37:40 -0300

    Consulta simples com query methods

--- src/main/java/com/willa/workshopmongo/services/PostService.java
+++ src/main/java/com/willa/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.willa.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> post  = postRepository.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Post not found"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
